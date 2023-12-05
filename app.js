0bad5a90ac6ececf5183979c9d0e9163b988f229
paolis28 <shinge@outlook.es>
2023-12-05 16:16:15 -0600

    Se modifico la api para los sockets

--- app.js
+++ app.js
@@ -5,17 +5,40 @@ import rutasRutina from './routes/rutinaRoute.js';
 import rutasClienteRutina from './routes/clienteRutinaRoute.js';
 import dotenv from "dotenv";
 import cors from 'cors';
+import {Server as SocketServer} from 'socket.io';
+import http from 'http';
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const server = http.createServer(app)
+const io = new SocketServer(server, {
+    cors: {
+        origin: '*'
+    }
+})
 app.use("/gimnasio/asistencia", rutasAsistencia);
 app.use("/gimnasio/rutina", rutasRutina);
 app.use("/gimnasio/clientesrutina", rutasClienteRutina);
 
+
 conn();
+
+io.on('connection', (socket) =>{
+    socket.on('message', (message, nickname) => {
+
+        newMessageCount++;
+    io.emit('newMessage', newMessageCount);
+        console.log(message)
+        socket.broadcast.emit('message', {
+            body: message,
+            from: nickname
+        })
+    })
+})
 
 const port = process.env.PORT;
 app.listen(port, ()=>{
